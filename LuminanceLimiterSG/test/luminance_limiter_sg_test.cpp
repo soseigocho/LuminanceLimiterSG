eb3ccb147adc81d91dc17cb9e64959de0b67a5db
soseigocho <October.Deep.blue.sky@gmail.com>
2023-03-16 04:06:40 +0900

    [upgrade] #13 task2|Include the sdk by git submodule.

--- LuminanceLimiterSG/test/luminance_limiter_sg_test.cpp
+++ LuminanceLimiterSG/test/luminance_limiter_sg_test.cpp
@@ -6,13 +6,4 @@ using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace luminance_limiter_sg_test
 {
-	TEST_CLASS(TestHello)
-	{
-	public:
-		TEST_METHOD(test_hello)
-		{
-			std::string hello = "Hello";
-			Assert::AreEqual(hello, luminance_limiter_sg::test_hello());
-		};
-	};
 }
