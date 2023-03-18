aa5fa41b8b852e4be12dad85cc5b08f81984c26c
soseigocho <October.Deep.blue.sky@gmail.com>
2023-03-19 03:49:36 +0900

    [downgrade] #17 task2 | Remove mock code.

--- LuminanceLimiterSG/src/luminance_limiter_sg.cpp
+++ LuminanceLimiterSG/src/luminance_limiter_sg.cpp
@@ -1,10 +1,1 @@
 #include "luminance_limiter_sg.h"
-
-#include <aviutl/filter.hpp>
-
-AviUtl::FilterPluginDLL filter{
-  .flag = AviUtl::FilterPlugin::Flag::AlwaysActive,
-  .name = "LuminanceLimiterSG",
-};
-
-auto __stdcall GetFilterTable() { return &filter; }
