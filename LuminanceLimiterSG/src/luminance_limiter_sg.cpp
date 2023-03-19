4310c9c39850acae64d4fca3695f24e4337b8009
soseigocho <October.Deep.blue.sky@gmail.com>
2023-03-20 03:50:18 +0900

    [upgrade] #13 task4 | Try concat constexpr char array and define by direct writing.

--- LuminanceLimiterSG/src/luminance_limiter_sg.cpp
+++ LuminanceLimiterSG/src/luminance_limiter_sg.cpp
@@ -2,9 +2,29 @@
 
 #include <aviutl/filter.hpp>
 
-AviUtl::FilterPluginDLL filter{
-	.flag = AviUtl::FilterPlugin::Flag::AlwaysActive,
+#include <algorithm>
+#include <array>
+
+
+namespace luminance_limiter_sg {
+
+	constexpr static inline auto track_n = 5u;
+	constexpr static inline auto track_name = std::array<const char*, track_n>{ "è„å¿", "è„å¿Ëáíl", "â∫å¿", "â∫å¿Ëáíl", "ÉQÉCÉì"};
+	constexpr static inline auto track_default = std::array<int32_t, track_n>{ 4096, -2048, 1024, 0, 0 };
+	constexpr static inline auto track_s = std::array<int32_t, track_n>{ 0, -4096, 0, 0, -4096 };
+	constexpr static inline auto track_e = std::array<int32_t, track_n>{ 4096, 0, 4096, 4096, 4096 };
+}
+
+
+constexpr AviUtl::FilterPluginDLL filter{
+	.flag = AviUtl::FilterPlugin::Flag::ExInformation,
 	.name = "LuminanceLimiterSG",
+	.track_n = luminance_limiter_sg::track_n,
+	.track_name = const_cast<const char**>(std::data(luminance_limiter_sg::track_name)),
+	.track_default = const_cast<int32_t*>(std::data(luminance_limiter_sg::track_default)),
+	.track_s = const_cast<int32_t*>(std::data(luminance_limiter_sg::track_s)),
+	.track_e = const_cast<int32_t*>(std::data(luminance_limiter_sg::track_e)),
+	.information = "LuminanceLimiterSG v0.2.0 by ëeêªåﬁí∑",
 };
 
 auto __stdcall GetFilterTable(void) {
