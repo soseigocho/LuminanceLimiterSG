598138edc1d57c7b281003c9c08bb98cf78a2f4a
soseigocho <October.Deep.blue.sky@gmail.com>
2023-03-21 00:48:40 +0900

    [upgrade] #13 task4 | Add part of the func_proc body.

--- LuminanceLimiterSG/src/luminance_limiter_sg.cpp
+++ LuminanceLimiterSG/src/luminance_limiter_sg.cpp
@@ -1,30 +1,74 @@
 #include "luminance_limiter_sg.h"
 
-#include <aviutl/filter.hpp>
-
-#include <algorithm>
 #include <array>
 
 
 namespace luminance_limiter_sg {
+	constexpr static inline auto name = "LuminanceLimiterSG";
+	constexpr static inline auto track_n = 7u;
+	constexpr static inline auto track_name =
+		std::array<const char*, track_n>{ "è„å¿", "è„å¿Ëáíl", "â∫å¿", "â∫å¿Ëáíl", "ÉQÉCÉì", "ñæÇÈÇ≥", "à√Ç≥"};
+	constexpr static inline auto track_default = std::array<int32_t, track_n>{ 4096, -256, 256, 0, 0, 0, 0 };
+	constexpr static inline auto track_s = std::array<int32_t, track_n>{ 0, -4096, 0, 0, -4096, -4096, -4096 };
+	constexpr static inline auto track_e = std::array<int32_t, track_n>{ 4096, 0, 4096, 4096, 4096, 4096, 4096 };
+	constexpr static inline auto information = "LuminanceLimiterSG v0.2.0 by ëeêªåﬁí∑";
 
-	constexpr static inline auto track_n = 5u;
-	constexpr static inline auto track_name = std::array<const char*, track_n>{ "è„å¿", "è„å¿Ëáíl", "â∫å¿", "â∫å¿Ëáíl", "ÉQÉCÉì"};
-	constexpr static inline auto track_default = std::array<int32_t, track_n>{ 4096, -2048, 1024, 0, 0 };
-	constexpr static inline auto track_s = std::array<int32_t, track_n>{ 0, -4096, 0, 0, -4096 };
-	constexpr static inline auto track_e = std::array<int32_t, track_n>{ 4096, 0, 4096, 4096, 4096 };
+	constexpr static inline auto y_max = 4096.0f;
+	constexpr static inline auto y_min = 0.0f;
+
+	NormalizedYBuffer::NormalizedYBuffer(AviUtl::FilterProcInfo* fpip)
+	{
+		const auto max_width = fpip->max_w;
+		const auto max_height = fpip->max_h;
+		this->buffer = std::vector(max_width * max_height, 0.0f);
+
+		const auto width = fpip->w;
+		const auto height = fpip->h;
+
+		AviUtl::PixelYC* row = nullptr;
+		auto buf_idx = 0;
+		for (auto y = 0; y < height; ++y)
+		{
+			row = static_cast<AviUtl::PixelYC*>(fpip->ycp_edit) + y * max_width;
+			for (auto x = 0; x < width; ++x)
+			{
+				buffer[buf_idx] = static_cast<float>((row + x)->y) / y_max;
+				buf_idx++;
+			}
+		}
+	}
+
+	NormalizedY NormalizedYBuffer::maximum() const
+	{
+		return *std::max_element(this->buffer.begin(), this->buffer.end());
+	}
+
+	NormalizedY NormalizedYBuffer::minimum() const
+	{
+		return *std::max_element(this->buffer.begin(), this->buffer.end());
+	}
+
+	static inline BOOL func_proc(AviUtl::FilterPlugin* fp, AviUtl::FilterProcInfo* fpip)
+	{
+		auto normalized_y_buffer = NormalizedYBuffer(fpip);
+		const auto orig_top_peak = normalized_y_buffer.maximum();
+		const auto orig_bottom_peak = normalized_y_buffer.minimum();
+		const auto gain = fp->track[4] / y_max;
+		normalized_y_buffer.pixelwise_map([=](NormalizedY pixel) {return pixel + gain; });
+		return true;
+	}
 }
 
 
 constexpr AviUtl::FilterPluginDLL filter{
 	.flag = AviUtl::FilterPlugin::Flag::ExInformation,
-	.name = "LuminanceLimiterSG",
+	.name = luminance_limiter_sg::name,
 	.track_n = luminance_limiter_sg::track_n,
 	.track_name = const_cast<const char**>(std::data(luminance_limiter_sg::track_name)),
 	.track_default = const_cast<int32_t*>(std::data(luminance_limiter_sg::track_default)),
 	.track_s = const_cast<int32_t*>(std::data(luminance_limiter_sg::track_s)),
 	.track_e = const_cast<int32_t*>(std::data(luminance_limiter_sg::track_e)),
-	.information = "LuminanceLimiterSG v0.2.0 by ëeêªåﬁí∑",
+	.information = luminance_limiter_sg::information,
 };
 
 auto __stdcall GetFilterTable(void) {
--- LuminanceLimiterSG/src/luminance_limiter_sg.h
+++ LuminanceLimiterSG/src/luminance_limiter_sg.h
@@ -1,3 +1,32 @@
 #pragma once
 
+#include <aviutl/filter.hpp>
+
+#include <algorithm>
+#include <concepts>
+#include <vector>
+
+
+namespace luminance_limiter_sg {
+	using NormalizedY = float;
+	class NormalizedYBuffer {
+	public:
+		NormalizedYBuffer(AviUtl::FilterProcInfo* fpip);
+
+		NormalizedY maximum() const;
+		NormalizedY minimum() const;
+
+		template<typename F>
+		BOOL pixelwise_map(F f) {
+			for (auto& elem:this->buffer)
+			{
+				elem = f(elem);
+			}
+			return true;
+		}
+	private:
+		std::vector<NormalizedY> buffer;
+	};
+}
+
 auto __stdcall GetFilterTable();
